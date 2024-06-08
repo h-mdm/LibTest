4def03ad54f1e6c7d16fbd305085bba96bead21a
mayorow <mayorow@gmail.com>
2024-06-08 12:33:31 +0400

    Added test of forcing configuration update

--- app/src/main/java/com/hmdm/libtest/MainActivity.java
+++ app/src/main/java/com/hmdm/libtest/MainActivity.java
@@ -2,7 +2,9 @@ package com.hmdm.libtest;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +12,23 @@ import com.hmdm.HeadwindMDM;
 
 public class MainActivity extends AppCompatActivity implements HeadwindMDM.EventHandler {
 
-    private String API_KEY = "get-key-at-h-mdm.com";
+    private String API_KEY = "c4Bz60gRwz";
     private TextView infoView;
+    private Button updateConfigButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         infoView = findViewById(R.id.info_view);
+        updateConfigButton = findViewById(R.id.update_config_button);
+        updateConfigButton.setOnClickListener(v -> {
+            try {
+                HeadwindMDM.getInstance().forceConfigUpdate();
+            } catch (Exception e) {
+                Toast.makeText(this, "Error updating config: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
